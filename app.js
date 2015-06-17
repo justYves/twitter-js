279f775b02028d88d57f16762ef1c54b3ea51de7
Yves Yuen <yves.j.yuen@gmail.com>
2015-06-17 12:53:45 -0400

    Added basic template index.html

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 var express = require("express");
 var app = express();
 var morgan = require("morgan");
+var swig = require("swig");
+
+swig.setDefaults({ cache: false });
 
 app.use(morgan("dev"));
+app.engine("html", swig.renderFile)
+
+
 
 app.listen(3000,function(){
 	console.log("server listening upon connection");
 });
 
+app.set("views",__dirname + "/views")
+app.set("view engine","html")
+
+
+var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+
 app.get("/",function(request,response){
-	response.status(200).send("Hello World!");
+	response.render("index", {title: 'Hall of Fame', people: people});
 })
 
-app.get("/news",function(request,response){
-	response.status(200).send("This is the news page!");
-})
+// app.get("/",function(request,response){
+// 	response.status(200).send("Hello World!");
+// })
+
+// app.get("/news",function(request,response){
+// 	response.status(200).send("This is the news page!");
+// })
 // app.send();
